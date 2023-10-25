839f0a8595e4c1f22a88983eb70a46fade345808
adikrisnaffa <adikrisnanugraha@gmail.com>
2023-10-25 07:32:47 +0700

    updated tables

--- dbo/Tables/EmpDetails.sql
+++ dbo/Tables/EmpDetails.sql
@@ -2,6 +2,7 @@
     [ID]           INT        NOT NULL,
     [EmpFirstName] NCHAR (10) NULL,
     [EmpLastName]  NCHAR (10) NULL,
+    [Email] NVARCHAR(100) NULL,
     [Department]   NCHAR (10) NULL,
     [Phone]        NCHAR (10) NULL,
     [Salary]       MONEY      NULL,
